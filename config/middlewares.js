94c33645511aa407aa9c89ffdaae8da3ba5f89f0
Вова Герман <vova.german1789@gmail.com>
2026-05-09 20:15:20 +0300

    fix allowed origins 2

--- config/middlewares.js
+++ config/middlewares.js
@@ -13,14 +13,24 @@ module.exports = [
     config: {
       enabled: true,
       headers: '*',
-      origin: [
-        'http://localhost:1337',
-        'http://localhost:5173',
-        /^http:\/\/192\.168\.\d+\.\d+:5173$/,
-        /^http:\/\/10\.\d+\.\d+\.\d+:5173$/,
-        'https://justworship.uk',       // Ваші старі домени
-        'https://new.justworship.uk'    // <--- ВАШ НОВИЙ ДОМЕН (без слеша в кінці!)
-      ]
+      origin: (ctx) => {
+        const requestOrigin = ctx.request.header.origin;
+        if (!requestOrigin) return false;
+        const allowed = [
+          'http://localhost:1337',
+          'http://localhost:5173',
+          'https://justworship.uk',
+          'https://new.justworship.uk',
+        ];
+        const lanRegex = [
+          /^http:\/\/192\.168\.\d+\.\d+:5173$/,
+          /^http:\/\/10\.\d+\.\d+\.\d+:5173$/,
+          /^http:\/\/172\.(1[6-9]|2\d|3[01])\.\d+\.\d+:5173$/,
+        ];
+        if (allowed.includes(requestOrigin)) return requestOrigin;
+        if (lanRegex.some((re) => re.test(requestOrigin))) return requestOrigin;
+        return false;
+      },
     }
   },
 ];
